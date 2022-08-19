9866af706f53f71d8be9990b02ffa3f35635559f
Leonid <leonovozhilov1967@gmail.com>
2022-08-19 13:50:07 +0300

    added glitch header, fixed some bugs

--- components/header/header.component.tsx
+++ components/header/header.component.tsx
@@ -1,78 +1,100 @@
-import Image from "next/image"
+import Image from "next/future/image"
 import { Link } from "react-scroll"
-import { FC } from "react"
+import { FC, useEffect } from "react"
+import { PowerGlitch } from "powerglitch"
+import { WHATSAPP_LINK, TG_LINK, VK_LINK } from "./header.constants"
 
 import logo from '../../resources/img/logo.webp'
-import headerpreview from '../../resources/img/header-preview.png'
+import glitchart from '../../resources/img/glitchimg.webp'
 
-const Header: FC = () => (
-    <div className="header">
-        <div className="header__main">
-            <ul className="header__main-nav">
-                <li id="firstli">
+const Header: FC = () =>{
+    useEffect(() => {
+        PowerGlitch.glitch('.glitch', {
+            timing: {
+                duration: 5000,
+                iterations: Infinity
+            },
+            glitchTimeSpan: {
+                start: 0.5,
+                end: 0.6
+            }
+        })
+    }), []
+
+    return (
+        <div className="header">
+            <div className="header__main">
+                <ul className="header__main-nav">
+                    <li id="firstli">
+                        <Link 
+                            to="services"
+                            spy={true}
+                            smooth={true}
+                            offset={0}
+                            duration={700}>
+                                Услуги
+                        </Link>
+                    </li>
+                    <li>
+                        <Link 
+                            to="gallery"
+                            spy={true}
+                            smooth={true}
+                            offset={0}
+                            duration={700}>
+                                Галерея
+                        </Link>
+                    </li>
+                    <li>
+                        <Link 
+                            to="contacts"
+                            spy={true}
+                            smooth={true}
+                            offset={0}
+                            duration={700}>
+                                Контакты
+                        </Link>
+                    </li>
+                </ul>
+                <h1>_MANICUTE_</h1>
+                <h3>Студия неординарных ногтей,<br/>услуги бровиста в Москве и<br/>Санкт-Петербурге</h3>
+                <div className="header__main-logo"><Image src={logo} alt="logo" width="120" height="120" /></div>
+                <div className="header__main-buttons">
+                    <a href={WHATSAPP_LINK}>
+                        <button><p>Написать в WhatsApp</p></button>
+                    </a>
+                    <a href={TG_LINK}>
+                        <button><p>Подписаться на Telegram</p></button>
+                    </a>
+                    <a href={VK_LINK}>
+                        <button><p>Подписаться на VK</p></button>
+                    </a>
                     <Link 
-                        to="services"
-                        spy={true}
-                        smooth={true}
-                        offset={0}
-                        duration={700}>
-                            Услуги
+                            to="services"
+                            spy={true}
+                            smooth={true}
+                            offset={0}
+                            duration={700}>
+                        <button><p>Услуги</p></button>
                     </Link>
-                </li>
-                <li>
-                    <Link 
-                        to="gallery"
-                        spy={true}
-                        smooth={true}
-                        offset={0}
-                        duration={700}>
-                            Галерея
-                    </Link>
-                </li>
-                <li>
-                    <Link 
-                        to="contacts"
-                        spy={true}
-                        smooth={true}
-                        offset={0}
-                        duration={700}>
-                            Контакты
-                    </Link>
-                </li>
-            </ul>
-            <h1>_MANICUTE_</h1>
-            <h3>Студия неординарных ногтей,<br/>услуги бровиста в Москве и<br/>Санкт-Петербурге</h3>
-            <div className="header__main-logo"><Image src={logo} alt="logo" /></div>
-            <div className="header__main-buttons">
-                <a href="#">
-                    <button><p>Написать в WhatsApp</p></button>
-                </a>
-                <a href="#">
-                    <button><p>Подписаться на Telegram</p></button>
-                </a>
-                <a href="#">
-                    <button><p>Подписаться на VK</p></button>
-                </a>
-                <a href="#">
-                    <button><p>Услуги</p></button>
-                </a>
-                <a href="#">
-                    <button><p>Акции</p></button>
-                </a>
-                <a href="#">
-                    <button><p>Как добраться</p></button>
-                </a>
+                    <a href="#">
+                        <button><p>Акции</p></button>
+                    </a>
+                    <a href="#">
+                        <button><p>Как добраться</p></button>
+                    </a>
+                </div>
             </div>
-        </div>
-        <div className="header__images">
-            <div className="header__images-logo"><Image src={logo} alt="logo" /></div>
-            <div className="header__images-art">
-                <div className="header__images-art-wrapper">
-                    <Image src={headerpreview} alt="art" quality="85" />
+            <div className="header__images">
+                <div className="header__images-logo"><Image src={logo} alt="logo" width="288" height="288" /></div>
+                <div className="header__images-art">
+                    <div className="header__images-art-wrapper">
+                        <Image src={glitchart} alt="glitchart" className="glitch" />
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-)
+    )
+} 
 
 export default Header
---
+++ components/header/header.constants.ts
@@ -0,0 +1,5 @@
+export const WHATSAPP_LINK = "https://api.whatsapp.com/send?phone=79523510669"
+
+export const TG_LINK = "https://t.me/MmaniCute"
+
+export const VK_LINK = "https://vk.com/manicutee"
