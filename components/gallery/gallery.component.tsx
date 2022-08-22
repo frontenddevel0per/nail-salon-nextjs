28d7e395d5c8f5b6cf461c937c70f2212272da11
Leonid <leonovozhilov1967@gmail.com>
2022-08-23 01:36:19 +0300

    added placeholder to gallery imgs

--- components/gallery/gallery.component.tsx
+++ components/gallery/gallery.component.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import { useState, useEffect, ReactNode, FC } from 'react';
-import { PAGINATION_STEP } from './gallery.constants';
+import { PAGINATION_STEP, PLACEHOLDER_BLUR } from './gallery.constants';
 
 import DB from '../../resources/DB/photos.json';
 
@@ -12,7 +12,7 @@ const Gallery: FC = () => {
         let arr = [];
         for (let i = 0; i < imagesCount; i++) {
             if (i < DB.length) {
-                arr.push(<Image key={i} src={DB[i].src} alt={DB[i].alt} width={250} height={250} objectFit="cover" />)
+                arr.push(<Image key={i} src={DB[i].src} alt={DB[i].alt} width={250} height={250} objectFit="cover" placeholder="blur" blurDataURL={PLACEHOLDER_BLUR} />)
             }
         }
         setImagesArr(arr);
