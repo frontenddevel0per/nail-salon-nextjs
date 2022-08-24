124ce0a4383c33dd5364d9b16d73b703150a1b8f
Leonid <leonovozhilov1967@gmail.com>
2022-08-24 16:39:37 +0300

    reworked icon component

--- components/icon/icon.component.tsx
+++ components/icon/icon.component.tsx
@@ -2,38 +2,20 @@ import { FC } from 'react'
 import { SHAPES } from './icon.constant'
 
 type IconProps = {
-    name: string,
+    name: keyof typeof SHAPES,
     size: number
 }
 
 const Icon: FC<IconProps> = ({name, size = 50}) => {
-    let iconPath: string = ""
-    const iconId: string = `${name}Icon`
-
-    switch (name) {
-        case "tg":
-            iconPath = SHAPES.TG;
-            break;
-        case "inst":
-            iconPath = SHAPES.INST;
-            break;
-        case "whatsapp":
-            iconPath = SHAPES.WA;
-            break;
-        case "vk":
-            iconPath = SHAPES.VK;
-            break;
-    }
-
     return (
         <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 50 50"
             width={`${size}px`}
             height={`${size}px`}
-            id={iconId}
+            id={`${name}Icon`}
         >
-            <path d={iconPath} />
+            <path d={SHAPES[name]} />
         </svg>
     )
 }
