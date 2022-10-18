e53d1a8d87b17b29046d64ae0ad458596a7babd0
Leonid <leonovozhilov1967@gmail.com>
2022-10-18 23:50:43 +0300

    layout: changed bg map to interactive map, changed site theme, some other small fixes

--- components/contacts/contacts.component.tsx
+++ components/contacts/contacts.component.tsx
@@ -1,54 +1,81 @@
-import { useState } from 'react';
-import Icon from '../icon/icon.component';
-import { FC } from 'react';
-import { ADDRESSES, ICONS_SIZE } from './contacts.constants';
-import { SOCIAL_LINKS } from '../shared.constant'
+import { useState } from "react";
+import Icon from "../icon/icon.component";
+import { FC } from "react";
+import { ADDRESSES, ICONS_SIZE } from "./contacts.constants";
+import { SOCIAL_LINKS } from "../shared.constant";
 
 type ContactsProps = {
-    setModal: (isOpened: boolean) => void;
-}
+  setModal: (isOpened: boolean) => void;
+};
 
-const Contacts: FC<ContactsProps> = ({setModal}) => {
-    const [activeMap, setActiveMap] = useState<string>("spb");
-    let address: string = "", mapClasses: string = "";
+const Contacts: FC<ContactsProps> = ({ setModal }) => {
+  const [activeMap, setActiveMap] = useState<string>("spb");
+  let address: string = "",
+    mapClasses: string = "";
 
-    switch (activeMap) {
-        case "msk":
-            address = ADDRESSES.MSK;
-            mapClasses = "contacts__map msk";
-            break;
-        case "spb":
-            address = ADDRESSES.SPB;
-            mapClasses = "contacts__map spb";
-            break;
-        }
-    ;
-
-    return (
-        <div className="contacts" id="contacts">
-            <div className="contacts__header">
-                <button className={(activeMap === "spb") ? "active" : undefined} onClick={() => setActiveMap("spb")}>СПб</button>
-                <button className={(activeMap === "msk") ? "active" : undefined} onClick={() => setActiveMap("msk")}>Мск</button>
-            </div>
-            <div className={mapClasses}></div>
-            <div className="contacts__mapinfo">
-                <h1>_ManiCute_</h1>
-                <p>{address}<br/>Пн-Вс, с 9:00 до 21:00</p>
-                <div className="contacts__mapinfo-icons">
-                    <a href={SOCIAL_LINKS.TG}>
-                        <Icon name="tg" size={ICONS_SIZE} />
-                    </a>
-                    <a href={SOCIAL_LINKS.WA}>
-                        <Icon name="whatsapp" size={ICONS_SIZE} />
-                    </a>
-                    <a href={SOCIAL_LINKS.VK}>
-                        <Icon name="vk" size={ICONS_SIZE} />
-                    </a>
-                </div>
-                <button onClick={() => setModal(true)}>Записаться</button>
-            </div>
+  switch (activeMap) {
+    case "msk":
+      address = ADDRESSES.MSK;
+      mapClasses = "contacts__map msk";
+      break;
+    case "spb":
+      address = ADDRESSES.SPB;
+      mapClasses = "contacts__map spb";
+      break;
+  }
+  return (
+    <div className="contacts" id="contacts">
+      <div className="contacts__header">
+        <button
+          className={activeMap === "spb" ? "active" : undefined}
+          onClick={() => setActiveMap("spb")}
+        >
+          СПб
+        </button>
+        <button
+          className={activeMap === "msk" ? "active" : undefined}
+          onClick={() => setActiveMap("msk")}
+        >
+          Мск
+        </button>
+      </div>
+      {activeMap === "spb" ? (
+        <iframe
+          src="https://yandex.ru/map-widget/v1/?um=constructor%3A7f14a301e121451c450945c4dc75847b6fcefedacdfdd4f59c4decd6308ce279&amp;source=constructor"
+          width="100%"
+          height="500"
+          frameBorder="0"
+        />
+      ) : (
+        <iframe
+          src="https://yandex.ru/map-widget/v1/?um=constructor%3Af8548a9ac2b91e766c9ea5c2a693b8e2bd4dfbe80847319c865b9c81e838316a&amp;source=constructor"
+          width="100%"
+          height="500"
+          frameBorder="0"
+        />
+      )}
+      <div className="contacts__mapinfo">
+        <h1>_ManiCute_</h1>
+        <p>
+          {address}
+          <br />
+          Пн-Вс, с 9:00 до 21:00
+        </p>
+        <div className="contacts__mapinfo-icons">
+          <a href={SOCIAL_LINKS.TG}>
+            <Icon name="tg" size={ICONS_SIZE} />
+          </a>
+          <a href={SOCIAL_LINKS.WA}>
+            <Icon name="whatsapp" size={ICONS_SIZE} />
+          </a>
+          <a href={SOCIAL_LINKS.VK}>
+            <Icon name="vk" size={ICONS_SIZE} />
+          </a>
         </div>
-    )
-}
+        <button onClick={() => setModal(true)}>Записаться</button>
+      </div>
+    </div>
+  );
+};
 
 export default Contacts;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,73 +1,82 @@
-import type { NextPage } from 'next'
-import Head from 'next/head';
-import { useState, useEffect } from 'react';
-import { isSafari } from 'react-device-detect';
+import type { NextPage } from "next";
+import Head from "next/head";
+import { useState, useEffect } from "react";
+import { isSafari } from "react-device-detect";
 
-import Preloader from '../components/preloader/perloader.component';
-import Header from '../components/header/header.component';
-import Services from '../components/services/services.component';
-import Gallery from '../components/gallery/gallery.component';
-import Contacts from '../components/contacts/contacts.component';
-import Footer from '../components/footer/footer.component';
-import ModalWindow from '../components/modal-window/modal-window.component';
+import Preloader from "../components/preloader/perloader.component";
+import Header from "../components/header/header.component";
+import Services from "../components/services/services.component";
+import Gallery from "../components/gallery/gallery.component";
+import Contacts from "../components/contacts/contacts.component";
+import Footer from "../components/footer/footer.component";
+import ModalWindow from "../components/modal-window/modal-window.component";
 
 const Home: NextPage = () => {
-  
   const [isModalActive, setModal] = useState<boolean>(false);
   const [showPreloader, setShowPreloader] = useState<boolean>(true);
   const [pageLoaded, setPageLoaded] = useState<boolean>(false);
   const [scrollWidth, setScrollWidth] = useState<number | null>(null);
 
-  const calcScrollbar = () : void => {
-    let div = document.createElement('div');
+  const calcScrollbar = (): void => {
+    let div = document.createElement("div");
 
-    div.style.overflowY = 'scroll';
-    div.style.width = '50px';
-    div.style.height = '50px';
+    div.style.overflowY = "scroll";
+    div.style.width = "50px";
+    div.style.height = "50px";
 
     // мы должны вставить элемент в документ, иначе размеры будут равны 0
     document.body.append(div);
     let value: number = div.offsetWidth - div.clientWidth;
     setScrollWidth(value);
     div.remove();
-  }
+  };
 
   useEffect(() => {
     calcScrollbar();
     setPageLoaded(true);
     setTimeout(() => setShowPreloader(false), 2500);
     if (isSafari) {
-      document.querySelector('body')?.classList.add('safari')
+      document.querySelector("body")?.classList.add("safari");
     }
-  }, [])
+  }, []);
 
   if (isModalActive) {
-    document.querySelector('html')!.style.overflowY = "hidden";
-    document.querySelector('body')!.style.marginRight = `${scrollWidth}px`;
+    document.querySelector("html")!.style.overflowY = "hidden";
+    document.querySelector("body")!.style.marginRight = `${scrollWidth}px`;
   } else if (!isModalActive && pageLoaded) {
-    document.querySelector('html')!.style.overflowY = "visible";
-    document.querySelector('body')!.style.marginRight = "0";
+    document.querySelector("html")!.style.overflowY = "visible";
+    document.querySelector("body")!.style.marginRight = "0";
   }
 
   return (
     <>
       <Head>
-        <meta name='keywords' content='маникюр, nail, маникюр спб, маникюр мск, маникюр питер, маникюр москва, ногти, маникюр 2022, педикюр, ногти маникюр, маникюр лето, маникюр лето 2022, маникюр дизайн, дизайн ногтей, ногти 2022, ногти дизайн новинки, manicute, _manicute_' />
-        <meta name='description' content='Студия неординарных ногтей, услуги бровиста в Москве и Санкт-Петербурге' key="desc" />
-        <meta property='og:title' content='Студия _Manicute_' />
-        <meta property='og:description' content='Студия неординарных ногтей, услуги бровиста в Москве и Санкт-Петербурге' />
-        <meta property='og:image' content='img/logo.jpg' />
+        <meta
+          name="keywords"
+          content="маникюр, nail, маникюр спб, маникюр мск, маникюр питер, маникюр москва, ногти, маникюр 2022, педикюр, ногти маникюр, маникюр осень, маникюр осень 2022, маникюр дизайн, дизайн ногтей, ногти 2022, ногти дизайн новинки, manicute, _manicute_"
+        />
+        <meta
+          name="description"
+          content="Студия неординарных ногтей, услуги бровиста в Москве и Санкт-Петербурге"
+          key="desc"
+        />
+        <meta property="og:title" content="Студия _Manicute_" />
+        <meta
+          property="og:description"
+          content="Студия неординарных ногтей, услуги бровиста в Москве и Санкт-Петербурге"
+        />
+        <meta property="og:image" content="img/logo.jpg" />
         <meta name="yandex-verification" content="34defb518339da6a" />
       </Head>
-      <Header/>
-      <Services setModal={setModal}/>
-      <Gallery/>
-      <Contacts setModal={setModal}/>
-      <Footer/>
-      {isModalActive ? <ModalWindow setModal={setModal}/> : null}
-      {showPreloader ? <Preloader/> : null}
+      <Header />
+      <Services setModal={setModal} />
+      <Gallery />
+      <Contacts setModal={setModal} />
+      <Footer />
+      {isModalActive ? <ModalWindow setModal={setModal} /> : null}
+      {showPreloader ? <Preloader /> : null}
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
